72c70dfcb2e8cd680309996d3e5967659b501357
Manoj <manojpatil9147@gmail.com>
2024-09-24 12:35:05 +0530

    added domain

--- KhataApp/settings.py
+++ KhataApp/settings.py
@@ -25,7 +25,7 @@ SECRET_KEY = "django-insecure-upu8^d(1!=8a4g$_7@h7gcam0yfljif^ycu-jc4fij+ymxvp@(
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = ['127.0.0.1','0000manoj000.pythonanywhere.com','khataapp.onrender.com']
+ALLOWED_HOSTS = ['127.0.0.1','0000manoj000.pythonanywhere.com','thecarzoom.pythonanywhere.com']
 
 FACTOR = 2 
 # Application definition

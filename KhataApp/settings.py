d2aa46796056bef78b2f34e73713fa9f16b9d47a
Manoj <manojpatil9147@gmail.com>
2024-10-02 19:19:29 +0530

    added logs

--- KhataApp/settings.py
+++ KhataApp/settings.py
@@ -75,6 +75,36 @@ TEMPLATES = [
     },
 ]
 
+LOGGING = {
+    'version': 1,
+    'disable_existing_loggers': False,
+    'formatters': {
+        'verbose': {
+            'format': '{levelname} {asctime} {module} {message}',
+            'style': '{',
+        },
+        'simple': {
+            'format': '{levelname} {message}',
+            'style': '{',
+        },
+    },
+    'handlers': {
+        'file': {
+            'level': 'ERROR',
+            'class': 'logging.FileHandler',
+            'filename': os.path.join(BASE_DIR, 'logs/django_error.log'),
+            'formatter': 'verbose',
+        },
+    },
+    'loggers': {
+        'django': {
+            'handlers': ['file'],
+            'level': 'ERROR',
+            'propagate': True,
+        },
+    },
+}
+
 WSGI_APPLICATION = "KhataApp.wsgi.application"
 
 
